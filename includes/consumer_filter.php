e38e930b57096e526c7bd31e5e3140b2e842d310
Xinyi Zhou <142810970+EZlla17@users.noreply.github.com>
2024-10-09 01:08:23 -0400

    Delete includes directory

--- includes/consumer_filter.php
+++
@@ -1,1 +0,0 @@
-<strong><a class= "tags-link" href="?<?php echo $tag_querystring?>"><?php echo$tag_name;?></a></strong>
--- includes/tags_link.php
+++
@@ -1,1 +0,0 @@
-<strong><a class= "tags-link" href="/admin?<?php echo $tag_querystring?>"><?php echo$tag_name;?></a></strong>
